c46e248a10b9f535fe6de08f0d441861c15e4073
RodrigoMenegat <rodrigoschuinski@gmail.com>
2020-03-06 23:59:37 -0300

    Adds force layout pre-processing; adds time-aware chart

--- viz/chart-1.js
+++ viz/chart-1.js
@@ -1,159 +1,123 @@
 // Based on https://bl.ocks.org/denjn5/6914f73f8bc3f009a875fa2bd11f81d8
 // and https://bl.ocks.org/mbostock/1667139
 
-function draw(disease) {
+function draw(diseases) {
 
-  d3.json(`../output/${disease}.json`).then(function(datapoints) {
-  // Load the data, then do the following...
+    Promise.all([
 
-    // Set the dimensions
-    let margin = { top: 10, left: 10, right: 10, bottom: 10},
-        height = 300 - margin.top - margin.bottom,
-        width = 300 - margin.left - margin.right;
+        d3.json(`${diseases[0]}-pre-processed.json`),
+        d3.json(`${diseases[1]}-pre-processed.json`),
+        d3.json(`${diseases[2]}-pre-processed.json`),
 
-    // Creates the svg
-    let svg = d3.select(`#${disease}`)
-      .append("svg")
-      .attr("height", height + margin.top + margin.bottom)
-      .attr("width", width + margin.left + margin.right)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    ]).then(function(datasets) {
 
-    // Apends a border
-    svg.append("rect")
-      .attr("fill", "none")
-      .attr("stroke", "#000")
-      .attr("stroke-width", .4)
-      .attr("opacity", 1)
-      .attr("width", width)
-      .attr("height", height)
+        // Set the dimensions
+        let margin = { top: 5, left: 5, right: 5, bottom: 5},
+            height = 300 - margin.top - margin.bottom,
+            width = 300 - margin.left - margin.right;
 
-    // Define a stratify utility to create an hierarchy
-    let stratify = d3.stratify()
-          .id(d => d.id)
-          .parentId(d => d.parent);
+        // Then, for each disease...
+        for (let [ index, disease ] of diseases.entries()) {
 
-    // Stratify the data
-    datapoints = stratify(datapoints);
+            // Select the data
+            var datapoints = datasets[index];
 
-    // Set up the hierarchy
-    let root = d3.hierarchy(datapoints);
-    let nodes = root.descendants();
-    let links = root.links();
+            // Create the svg
+            let svg = d3.select(`#${disease}`)
+                .append("svg")
+                .attr("height", height + margin.top + margin.bottom)
+                .attr("width", width + margin.left + margin.right)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        
+            // Apends a border
+            svg.append("rect")
+                .attr("fill", "none")
+                .attr("stroke", "#000")
+                .attr("stroke-width", .4)
+                .attr("opacity", 1)
+                .attr("width", width)
+                .attr("height", height)
 
-    // Declare d3 layout and stop it
-    let simulation = d3.forceSimulation(nodes)
-        .force('link', d3.forceLink(links).distance(1))
-        .force('charge', d3.forceManyBody().distanceMax(20))
-        .force('center', d3.forceCenter(width / 2, height / 2))
-        .stop();
+            let circles = svg.selectAll('circle')
+                .data(datapoints)
+                .enter()
+                .append('circle')
+                .attr('r', 0)
+                .attr("data-generation", d => d.generation)
+                .attr("data-disease", d => d.virusName.toLowerCase())
+                .attr('cx', d => d.cx)
+                .attr('cy', d => d.cy)
+                .attr("fill", "#ffbaba")
+                .style('stroke-width', '0px')
+                .attr('class', 'hidden')
 
-    d3.timeout(function() {
+            // Start revealing the chart, slowly
+            // Addapted from https://stackoverflow.com/questions/43382652/d3-set-elements-to-transition-in-order
 
-    // See https://github.com/d3/d3-force/blob/master/README.md#simulation_tick
-    for (var i = 0, n = Math.ceil(Math.log(simulation.alphaMin()) / Math.log(1 - simulation.alphaDecay())); i < n; ++i) {
-      simulation.tick();
-    }
+            // Create circle selections
+            let circleGenerations = [ 
+                "[data-generation=\"1\"]",
+                "[data-generation=\"2\"]",
+                "[data-generation=\"3\"]",
+                "[data-generation=\"4\"]",
+                "[data-generation]" 
+            ]
 
-    // Add the elements
-    // let lines = svg.selectAll('line')
-    //              .data(links)
-    //              .enter()
-    //              .append('line')
-    //              .attr("data-generation", d => d.source.data.data.generation + 1)
-    //              .attr('x1', d => d.source.x)
-    //              .attr('y1', d => d.source.y)
-    //              .attr('x2', d => d.target.x)
-    //              .attr('y2', d => d.target.y)
-    //              .attr('class', 'hidden')
-
-    let circles = svg.selectAll('circle')
-                   .data(nodes)
-                   .enter()
-                   .append('circle')
-                   .attr('r', 0)
-                   .attr("data-generation", d => d.data.data.generation)
-                   .attr('cx', d => d.x)
-                   .attr('cy', d => d.y)
-                   .attr("fill", "#ffbaba")
-                   .style('stroke-width', '0px')
-                   .attr('class', 'hidden')
-
-
-
-
-    // Start revealing the chart, slowly
-    // Addapted from https://stackoverflow.com/questions/43382652/d3-set-elements-to-transition-in-order
-
-      // Create circle selections
-      let circleGenerations = [ "[data-generation=\"1\"]",
-                                "[data-generation=\"2\"]",
-                                "[data-generation=\"3\"]",
-                                "[data-generation=\"4\"]",
-                                "[data-generation=\"5\"]",
-                                "[data-generation]" ]
-
-
-      function repeat() {
-
-        console.log("repeat");
-
-        for (let i = 0 ; i < circleGenerations.length; i++) {
-
-          currentGeneration = circleGenerations[i];
-
-          let repeatTrigger = true;
-
-          if (currentGeneration != "[data-generation]") {
-
-            d3.selectAll(currentGeneration)
-              .transition().duration(500).delay(i * 1500)
-              .attr("r", 4)
-              .style('stroke-width', '2px')
-              .style("fill", "#ffbaba")
-              .style("opacity", 1)
-              .attr("class", "active")
-              .transition().duration(500)
-              .attr("r", 3)
-              .style('stroke-width', '0px')
-              .transition().duration(500)
-              .style("fill", "#aeaeee")
-              .attr("class", "inactive");
-
-          }
-
-          else {
-
-            d3.selectAll(currentGeneration)
-              .transition().duration(1000).delay(i * 1700)
-              .style("opacity", 0)
-              .attr("class", "hidden")
-              .on("end", function() {
+            function repeat() {
                 
-                // Call the repeat function only once
-                if (repeatTrigger) {
-                  repeatTrigger = false;
-                  repeat();
+                for (let i = 0 ; i < circleGenerations.length; i++) {
+                
+                    currentGeneration = circleGenerations[i];
+                
+                    let repeatTrigger = true;
+                
+                    if (currentGeneration != "[data-generation]") {
+                
+                        d3.selectAll(currentGeneration)
+                            .transition().duration(500).delay(i * 1500)
+                            .attr("r", 4)
+                            .style('stroke-width', '2px')
+                            .style("fill", "#ffbaba")
+                            .style("opacity", 1)
+                            .attr("class", "active")
+                            .transition().duration(500)
+                            .attr("r", 3)
+                            .style('stroke-width', '0px')
+                            .transition().duration(500)
+                            .style("fill", "#aeaeee")
+                            .attr("class", "inactive");
+                
+                    }
+                
+                    else {
+                    
+                        d3.selectAll(currentGeneration)
+                            .transition().duration(1000).delay(i * 1700)
+                            .style("opacity", 0)
+                            .attr("class", "hidden")
+                            .on("end", function() {
+                                
+                                // Call the repeat function only once
+                                if (repeatTrigger) {
+                                    repeatTrigger = false;
+                                    repeat();
+                                }
+                    
+                            })
+
+                    }
+
                 }
+                  
+            }
 
-              })
-          }
-
+            repeat();
 
         }
-
-      }
-
-      repeat();
-
-
+    
     });
-
-  });
-
-
+  
 }
-
-draw("h1n1");
-draw("covid-19");
-draw("smallpox");
+  
+draw([ "covid-19", "h1n1", "measles" ]);
---
+++ viz/pre-process-force.js
@@ -0,0 +1,117 @@
+/* This script uses d3.js force package to compute
+coordinates for the elements in our charts. By doing this,
+we can simply load the pre-processed data in our other functions,
+without computing the same thing again and again and again
+every time we load the page */
+
+
+// Taken from https://stackoverflow.com/questions/34156282/how-do-i-save-json-to-local-text-file
+function download(content, fileName, contentType) {
+    var a = document.createElement("a");
+    var file = new Blob([content], {type: contentType});
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+}
+
+Promise.all([
+
+    d3.json(`../output/covid-19.json`),
+    d3.json(`../output/h1n1.json`),
+    d3.json(`../output/smallpox.json`),
+    d3.json(`../output/measles.json`)
+
+]).then(function(datapointsCollection) {
+
+    for (let [index, datapoints] of datapointsCollection.entries()) {
+
+        // Define a stratify utility to create an hierarchy
+        let stratify = d3.stratify()
+            .id(d => d.id)
+            .parentId(d => d.parent);
+
+        // Stratify the data
+        datapoints = stratify(datapoints);
+    
+        // Set up the hierarchy
+        let root = d3.hierarchy(datapoints);
+        let nodes = root.descendants();
+        let links = root.links();
+
+        // Set the future chart dimensions
+        let margin = { top: 10, left: 10, right: 10, bottom: 10},
+            height = 300 - margin.top - margin.bottom,
+            width = 300 - margin.left - margin.right;
+
+        var distanceMax = 20;
+            if (index == 3) { // Measles
+                distanceMax = 5;
+            }
+        
+        // Declare d3 layout and stop it
+        let simulation = d3.forceSimulation(nodes)
+            .force('link', d3.forceLink(links).distance(1))
+            .force('charge', d3.forceManyBody().distanceMax(distanceMax))
+            .force('center', d3.forceCenter(width / 2, height / 2))
+            .stop();
+
+        // Run the simulation
+
+        for (var i = 0, n = Math.ceil(Math.log(simulation.alphaMin()) / Math.log(1 - simulation.alphaDecay())); i < n; ++i) {
+            simulation.tick();
+
+        }
+
+        // Parse the output to keep only what we need
+        let outputData = [ ]
+
+        for (let node of nodes) {
+
+            var newNode = { }
+            
+            newNode.cx = node.x;
+            newNode.cy = node.y;
+            newNode.id = node.data.data.id;
+            newNode.generation = node.data.data.generation;
+            newNode.virusName = node.data.data.virus_name;
+            
+            outputData.push(newNode);
+            
+        }
+
+        // Show me the output
+        console.log(outputData);
+
+        // Save it
+        let filename = ""
+
+        if (index == 0) {
+
+            filename = "covid-19-pre-processed.json"
+
+        }
+
+        else if (index == 1) {
+
+            filename = "h1n1-pre-processed.json"
+
+        }
+
+        else if (index == 2) {
+
+            filename = "smallpox-pre-processed.json"
+
+        }
+
+        else if (index == 3) {
+
+            filename = "measles-pre-processed.json"
+
+        }
+
+        outputData = JSON.stringify(outputData);
+        download(outputData, filename, 'text-plain')
+        
+    }
+
+});
